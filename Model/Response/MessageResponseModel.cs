6597257b42c434e6a958bb1ef7afd564a3b9bfaf
siutin <osiutino@gmail.com>
2011-12-03 04:14:46 +0800

    create MessageResponseModel

---
+++ Model/Response/MessageResponseModel.cs
@@ -0,0 +1,17 @@
+namespace Nexmo_CSharp_lib.Model.Response
+{
+    public class MessageResponseModel
+    {   
+        public string Status;
+
+        public string MessageId;
+      
+        public string ClientRef;
+
+        public string RemainingBalance;
+     
+        public string MessagePrice;
+
+        public string ErrorText;
+    }
+}

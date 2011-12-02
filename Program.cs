a8554d02d4bb8f8653afeac3952ba7fa69cfa156
siutin <osiutino@gmail.com>
2011-12-03 06:07:39 +0800

    added a example of sending a text message in json

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Common.Logging;
+using Nexmo_CSharp_lib.Model.Request;
 
 namespace Nexmo_CSharp_lib
 {
@@ -14,8 +15,29 @@ namespace Nexmo_CSharp_lib
         static void Main(string[] args)
         {
             Log.Info("::Nexmo::");
-            Log.Info("Logging Test");
+            
+            JsonExample();
+
             Console.ReadLine();
+        }
+
+        private static void JsonExample()
+        {
+            Log.Info("- JsonExample -");
+
+            var textModel = new TextRequestModel {Text = "HelloWorld!"};
+            var builder = new RequestModelBuilder();
+         
+            var requestModel = builder.Create("{USERNAME}", "{PASSWORD}", "{FROM}", "{TO}", textModel);
+         
+            var nexmo = new Nexmo();
+            
+            var responseModel = nexmo.Send(requestModel, ResponseObjectType.Json);
+
+            Log.Info("- Result -");
+            responseModel.ReadObject();
+
+            Log.Info("- End -");
         }
     }
 }

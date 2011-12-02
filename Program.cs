fb76d3267e1cbab6746f9ccdbebe692b7eeeb41e
siutin <osiutino@gmail.com>
2011-12-03 05:45:41 +0800

    simple logging test , fixed a startup crash by using .Net Framework v4.0

--- Program.cs
+++ Program.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Common.Logging;
 
 namespace Nexmo_CSharp_lib
 {
     class Program
     {
+        protected static ILog Log = LogManager.GetLogger(typeof(Program));
+
+
         static void Main(string[] args)
         {
+            Log.Info("::Nexmo::");
+            Log.Info("Logging Test");
+            Console.ReadLine();
         }
     }
 }

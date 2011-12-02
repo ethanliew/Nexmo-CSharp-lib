a50eebcfd73b5d69cf41c99dc7f03533ac4ca087
siutin <osiutino@gmail.com>
2011-12-03 07:06:49 +0800

    fixed TextRequestSerializer error

--- Serializer/TextRequestSerializer.cs
+++ Serializer/TextRequestSerializer.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Nexmo_CSharp_lib.Interface;
 using Nexmo_CSharp_lib.Model.Request;
 
@@ -7,10 +8,21 @@ namespace Nexmo_CSharp_lib.Serializer
     {
         public string Serizalize(TextRequestModel o)
         {
-            return
-                string.Format(
-                    "&text={0}&status-report-req={1}&client-ref={2}&network-code={3}&vcard={4}&vcal={5}&ttl={6}", o.Text,
-                    o.StatusReportReq, o.ClientRef, o.NetworkCode, o.VCard, o.VCal, o.Ttl);
+            var sb = new StringBuilder();
+            sb.AppendFormat("&text={0}", o.Text);
+            if (o.StatusReportReq != null)
+                sb.AppendFormat("&status-report-req={0}",o.StatusReportReq);
+            if(o.ClientRef !=null)
+                sb.AppendFormat("&client-ref={0}", o.ClientRef);
+            if(o.NetworkCode !=null)
+                sb.AppendFormat("&network-code={0}", o.NetworkCode);
+            if(o.VCard !=null)
+                sb.AppendFormat("&vcard={0}", o.VCard);
+            if(o.VCal !=null)
+                sb.AppendFormat("&vcal={0}", o.VCal);
+            if(o.Ttl !=null)
+                sb.AppendFormat("&ttl={0}", o.Ttl);
+            return sb.ToString();
         }
     }
 }

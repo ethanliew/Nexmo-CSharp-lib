bff7ef9536e07708d8697cacaa861d895dd0cfca
siutin <osiutino@gmail.com>
2011-12-03 03:46:06 +0800

    first create project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nexmo_CSharp_lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

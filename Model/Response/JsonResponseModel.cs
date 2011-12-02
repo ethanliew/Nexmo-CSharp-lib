79b6505891426e3136fb6a1e4f001f7f076b0289
siutin <osiutino@gmail.com>
2011-12-03 04:43:18 +0800

    make JsonResponseModel ,MessageResponseModel  as a JsonObject

--- Model/Response/JsonResponseModel.cs
+++ Model/Response/JsonResponseModel.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Nexmo_CSharp_lib.Interface;
 
 namespace Nexmo_CSharp_lib.Model.Response
 {
-    public class JsonResponseModel :IResponseModel
-    {      
+    [JsonObject(MemberSerialization.OptOut)]
+    public class JsonResponseModel : IResponseModel
+    {
+        [JsonProperty(PropertyName = "message-count")]
         public string MessageCount;
-  
+
+        [JsonProperty(PropertyName = "messages")]
         public ICollection<MessageResponseModel> MessageModels;
     }
 }
--- Model/Response/MessageResponseModel.cs
+++ Model/Response/MessageResponseModel.cs
@@ -1,17 +1,26 @@
+using Newtonsoft.Json;
+
 namespace Nexmo_CSharp_lib.Model.Response
 {
     public class MessageResponseModel
-    {   
+    {
+       
+        [JsonProperty(PropertyName = "status")]
         public string Status;
 
+        [JsonProperty(PropertyName = "message-id")]
         public string MessageId;
-      
+   
+        [JsonProperty(PropertyName = "client-ref")]
         public string ClientRef;
 
+        [JsonProperty(PropertyName = "remaining-balance")]
         public string RemainingBalance;
      
+        [JsonProperty(PropertyName = "message-price")]
         public string MessagePrice;
-
+    
+        [JsonProperty(PropertyName = "error-text")]
         public string ErrorText;
     }
 }

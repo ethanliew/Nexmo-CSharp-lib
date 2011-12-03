2d4d53ddd8b34abca3c84180d761ea8e7676b19c
siutin <osiutino@gmail.com>
2011-12-03 15:35:54 +0800

    rename XmlResponseserializer to XmlResponseDeserializer

---
+++ Serializer/XmlResponseDeserializer.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System.Xml.Serialization;
+using Nexmo_CSharp_lib.Interface;
+using Nexmo_CSharp_lib.Model.Response;
+
+namespace Nexmo_CSharp_lib.Serializer
+{
+    public class XmlResponseDeserializer : IResponseDeserializer<XmlResponseModel>
+    {
+        public XmlResponseModel Deserialize(Stream stream)
+        {
+            var xmlSerializer = new XmlSerializer(typeof (XmlResponseModel));
+            var nexmoResponseModel = (XmlResponseModel) xmlSerializer.Deserialize(stream);
+            return nexmoResponseModel;
+        }
+    }
+}
